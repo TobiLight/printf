7f7adc6a977f590eb3b58a404dceaf742ef44bbb
TobiLight <jgetitdonefast@gmail.com>
2023-03-28 14:56:50 -0700

    Handle custom coversion specifiers for 'r': Prints reversed string

--- print_func3.c
+++ print_func3.c
@@ -94,3 +94,46 @@ int print_pointer(va_list types, char buffer[],
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
 }
+
+/**
+ * print_reverse - Prints reverse string.
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of chars printed
+ */
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	(void) buffer;
+	(void) flags;
+	(void) width;
+	(void) size;
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		(void) precision
+
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
+
+		write(1, &z, 1);
+		count++;
+	}
+	return (count);
+}
