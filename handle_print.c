0da1dc34635ec0b3bd9a3b4884f0162d4659241b
TobiLight <jgetitdonefast@gmail.com>
2023-03-28 06:42:11 -0700

    Handle conversion specifiers for S and non-printable characters.

--- handle_print.c
+++ handle_print.c
@@ -25,7 +25,7 @@ int print_handler(const char *fmt, int *ind, va_list list, char buffer[],
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'o', print_octal}, {'u', print_unsigned}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper},
+		{'X', print_hexa_upper}, {'S', print_non_printable},
 		{'\0', NULL}};
 	for (i = 0; fmt_specifiers[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_specifiers[i].fmt)
